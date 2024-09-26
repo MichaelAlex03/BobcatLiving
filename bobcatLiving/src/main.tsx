d58f32b6ae30e9f131479ec661b46ee02d040345
aychar <58487401+hrfarmer@users.noreply.github.com>
2024-09-25 23:55:02 -0500

    clean up router

--- bobcatLiving/src/main.tsx
+++ bobcatLiving/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./Pages/home.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </>
+  )
+);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
