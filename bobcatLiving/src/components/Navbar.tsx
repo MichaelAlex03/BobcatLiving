53623a5c48ac1aab2b69a5e53c0a5805d7aa71a0
aychar <58487401+hrfarmer@users.noreply.github.com>
2024-10-27 17:35:30 -0500

    add boilerplate for apartments page

--- bobcatLiving/src/components/Navbar.tsx
+++ bobcatLiving/src/components/Navbar.tsx
@@ -1,42 +1,65 @@
-import React, { useState } from 'react'
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <div>
       <header className="flex justify-center text-white py-6 px-8 md:px-32 bg-background drop-shadow-md">
-
-        <ul className='hidden xl:flex lg:flex items-center gap-12 font-semibold text-base'>
-          <li><a href="/" className='nav'>Home</a></li>
-          <li><a href="/" className='nav'>View Apartments</a></li>
-          <li><a href="/" className='nav '>Contact</a></li>
+        <ul className="hidden xl:flex lg:flex items-center gap-12 font-semibold text-base">
+          <li>
+            <a href="/" className="nav">
+              Home
+            </a>
+          </li>
+          <li>
+            <Link to={"/apartments"} className="nav">
+              View Apartments
+            </Link>
+          </li>
+          <li>
+            <a href="/" className="nav ">
+              Contact
+            </a>
+          </li>
         </ul>
 
-
-        <div className='hidden xl:flex lg:flex ml-auto gap-8 font-semibold' > 
-          <button className='py-2 px-8 hover:bg-background hover:text-white hover:border-bg-background border-2 bg-bar text-white rounded-md transition-all cursor-pointer hover:bg-blue-70'>Log In</button>
-          <button className='py-2 px-8 text-white hover:bg-bar hover:text-white border-bg-bar border-2 rounded-md transition-all cursor-pointer'>Sign Up</button>
+        <div className="hidden xl:flex lg:flex ml-auto gap-8 font-semibold">
+          <button className="py-2 px-8 hover:bg-background hover:text-white hover:border-bg-background border-2 bg-bar text-white rounded-md transition-all cursor-pointer hover:bg-blue-70">
+            Log In
+          </button>
+          <button className="py-2 px-8 text-white hover:bg-bar hover:text-white border-bg-bar border-2 rounded-md transition-all cursor-pointer">
+            Sign Up
+          </button>
         </div>
 
-        <div className='lg:hidden flex items-center w-full'>
-          <span className='text-xl font-bold block mr-auto'>BobcatLiving</span>
-          <i className='bx bx-menu text-5xl cursor-pointer flex justify-start ' onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+        <div className="lg:hidden flex items-center w-full">
+          <span className="text-xl font-bold block mr-auto">BobcatLiving</span>
+          <i
+            className="bx bx-menu text-5xl cursor-pointer flex justify-start "
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          ></i>
         </div>
-       
       </header>
 
-      <div className={`absolute xl:hidden lg:hidden m:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold transform transition-transform' 
-      ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`} style={{transition: 'transform 0.3s ease, opacity 0.3s ease'}}>
-        <li className='list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all curse-pointer'>Home</li>
-        <li className='list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all curse-pointer'>View Apartments</li>
-        <li className='list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all curse-pointer'>Contact</li>
-
+      <div
+        className={`absolute xl:hidden lg:hidden m:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold transform transition-transform' 
+      ${isMenuOpen ? "opacity-100" : "opacity-0"}`}
+        style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}
+      >
+        <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all curse-pointer">
+          Home
+        </li>
+        <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all curse-pointer">
+          View Apartments
+        </li>
+        <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all curse-pointer">
+          Contact
+        </li>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- bobcatLiving/src/main.tsx
+++ bobcatLiving/src/main.tsx
@@ -8,17 +8,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import ApartmentsPage from "./Pages/apartments.tsx";
+import Home from "./Pages/home.tsx";
 import Login from "./Pages/login.tsx";
-import ApartmentCard from "./components/apartmentCard.tsx";
-import Home from "./Pages/home.tsx"
-
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/apartments" element={<ApartmentsPage />} />
     </>
   )
 );
